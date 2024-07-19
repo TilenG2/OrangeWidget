fbe7036a2505a037a6c2f6b95977ca318b67c118
Tilen_G <43862401+TilenG2@users.noreply.github.com>
2024-07-19 09:03:03 +0200

    code work 90% of time

--- Uncertaintan/trees/owuncertantytree.py
+++ Uncertaintan/trees/owuncertantytree.py
@@ -11,271 +11,16 @@ from AnyQt.QtCore import Qt
 from Orange.data import Table
 from Orange.widgets import gui
 from Orange.widgets.settings import Setting
-from Orange.widgets.data.owcsvimport import pandas_to_table
 from Orange.widgets.utils.localization import pl
 from Orange.classification import _tree_scorers
 from Orange.widgets.utils.owlearnerwidget import OWBaseLearner
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.classification import Learner
-from Orange.base import TreeModel as TreeModelInterface
-from Orange.tree import Node, DiscreteNode, MappedDiscreteNode, NumericNode
+from Orange.tree import Node, DiscreteNode, MappedDiscreteNode, NumericNode, TreeModel
 from Orange.statistics import distribution, contingency
-from Orange.modelling import Fitter
-
-class UncertanTreeModel(TreeModelInterface):
-    """
-    Tree classifier with proper handling of nominal attributes and binarization
-    and the interface API for visualization.
-    """
-
-    def __init__(self, data, root):
-        super().__init__(data.domain)
-        self.instances = data
-        self.root = root
-
-        self._values = self._thresholds = self._code = None
-        self._compile()
-        self._compute_descriptions()
-
-    def _prepare_predictions(self, n):
-        rootval = self.root.value
-        return np.empty((n,) + rootval.shape, dtype=rootval.dtype)
-
-    def get_values_by_nodes(self, X):
-        """Prediction that does not use compiled trees; for demo only"""
-        n = len(X)
-        y = self._prepare_predictions(n)
-        for i in range(n):
-            x = X[i]
-            node = self.root
-            while True:
-                child_idx = node.descend(x)
-                if np.isnan(child_idx):
-                    break
-                next_node = node.children[child_idx]
-                if next_node is None:
-                    break
-                node = next_node
-            y[i] = node.value
-        return y
-
-    def get_values_in_python(self, X):
-        """Prediction with compiled code, but in Python; for demo only"""
-        n = len(X)
-        y = self._prepare_predictions(n)
-        for i in range(n):
-            x = X[i]
-            node_ptr = 0
-            while self._code[node_ptr]:
-                val = x[self._code[node_ptr + 2]]
-                if np.isnan(val):
-                    break
-                child_ptrs = self._code[node_ptr + 3:]
-                if self._code[node_ptr] == 3:
-                    node_idx = self._code[node_ptr + 1]
-                    next_node_ptr = child_ptrs[int(val > self._thresholds[node_idx])]
-                else:
-                    next_node_ptr = child_ptrs[int(val)]
-                if next_node_ptr == -1:
-                    break
-                node_ptr = next_node_ptr
-            node_idx = self._code[node_ptr + 1]
-            y[i] = self._values[node_idx]
-        return y
-
-    def get_values(self, X):
-        from Orange.classification import _tree_scorers
-        if sp.isspmatrix_csc(X):
-            func = _tree_scorers.compute_predictions_csc
-        elif sp.issparse(X):
-            func = _tree_scorers.compute_predictions_csr
-            X = X.tocsr()
-        else:
-            func = _tree_scorers.compute_predictions
-        return func(X, self._code, self._values, self._thresholds)
-
-    def predict(self, X):
-        predictions = self.get_values(X)
-        if self.domain.class_var.is_continuous:
-            return predictions[:, 0]
-        else:
-            sums = np.sum(predictions, axis=1)
-            # This can't happen because nodes with 0 instances are prohibited
-            # zeros = (sums == 0)
-            # predictions[zeros] = 1
-            # sums[zeros] = predictions.shape[1]
-            return predictions / sums[:, np.newaxis]
-
-    def node_count(self):
-        def _count(node):
-            return 1 + sum(_count(c) for c in node.children if c)
-        return _count(self.root)
-
-    def depth(self):
-        def _depth(node):
-            return 1 + max((_depth(child) for child in node.children if child),
-                           default=0)
-        return _depth(self.root) - 1
-
-    def leaf_count(self):
-        def _count(node):
-            return not node.children or \
-                   sum(_count(c) if c else 1 for c in node.children)
-        return _count(self.root)
-
-    def get_instances(self, nodes):
-        indices = self.get_indices(nodes)
-        if indices is not None:
-            return self.instances[indices]
-
-    def get_indices(self, nodes):
-        subsets = [node.subset for node in nodes]
-        if subsets:
-            return np.unique(np.hstack(subsets))
-
-    @staticmethod
-    def climb(node):
-        while node:
-            yield node
-            node = node.parent
-
-    @classmethod
-    def rule(cls, node):
-        rules = []
-        used_attrs = set()
-        for node in cls.climb(node):
-            if node.parent is None or node.parent.attr_idx in used_attrs:
-                continue
-            parent = node.parent
-            attr = parent.attr
-            name = attr.name
-            if isinstance(parent, NumericNode):
-                lower, upper = node.condition
-                if upper is None:
-                    rules.append("{} > {}".format(name, attr.repr_val(lower)))
-                elif lower is None:
-                    rules.append("{} ≤ {}".format(name, attr.repr_val(upper)))
-                else:
-                    rules.append("{} < {} ≤ {}".format(
-                        attr.repr_val(lower), name, attr.repr_val(upper)))
-            else:
-                rules.append("{}: {}".format(name, node.description))
-            used_attrs.add(node.parent.attr_idx)
-        return rules
-
-    def print_tree(self, node=None, level=0):
-        """String representation of tree for debug purposees"""
-        if node is None:
-            node = self.root
-        res = ""
-        for child in node.children:
-            res += ("{:>20} {}{} {}\n".format(
-                str(child.value), "    " * level, node.attr.name,
-                child.description))
-            res += self.print_tree(child, level + 1)
-        return res
-
-    NODE_TYPES = [Node, DiscreteNode, MappedDiscreteNode, NumericNode]
-
-    def _compile(self):
-        def _compute_sizes(node):
-            nonlocal nnodes, codesize
-            nnodes += 1
-            codesize += 2  # node type + node index
-            if isinstance(node, MappedDiscreteNode):
-                codesize += len(node.mapping)
-            if node.children:
-                codesize += 1 + len(node.children)  # attr index + children ptrs
-                for child in node.children:
-                    if child is not None:
-                        _compute_sizes(child)
-
-        def _compile_node(node):
-            from Orange.classification._tree_scorers import NULL_BRANCH
-
-            # The node is compile into the following code (np.int32)
-            # [0] node type: index of type in NODE_TYPES)
-            # [1] node index: serves as index into values and thresholds
-            # If the node is not a leaf:
-            #     [2] attribute index
-            # This is followed by an array of indices of the code for children
-            # nodes. The length of this array is 2 for numeric attributes or
-            # **the number of attribute values** for discrete attributes
-            # This is different from the number of branches if discrete values
-            # are mapped to branches
-
-            # Thresholds and class distributions are stored in separate
-            # 1-d and 2-d array arrays of type np.float, indexed by node index
-            # The lengths of both equal the node count; we would gain (if
-            # anything) by not reserving space for unused threshold space
-            if node is None:
-                return NULL_BRANCH
-            nonlocal code_ptr, node_idx
-            code_start = code_ptr
-            self._code[code_ptr] = self.NODE_TYPES.index(type(node))
-            self._code[code_ptr + 1] = node_idx
-            code_ptr += 2
-
-            self._values[node_idx] = node.value
-            if isinstance(node, NumericNode):
-                self._thresholds[node_idx] = node.threshold
-            node_idx += 1
-
-            # pylint: disable=unidiomatic-typecheck
-            if type(node) == Node:
-                return code_start
-
-            self._code[code_ptr] = node.attr_idx
-            code_ptr += 1
-
-            jump_table_size = 2 if isinstance(node, NumericNode) \
-                else len(node.attr.values)
-            jump_table = self._code[code_ptr:code_ptr + jump_table_size]
-            code_ptr += jump_table_size
-            child_indices = [_compile_node(child) for child in node.children]
-            if isinstance(node, MappedDiscreteNode):
-                jump_table[:] = np.array(child_indices)[node.mapping]
-            else:
-                jump_table[:] = child_indices
-
-            return code_start
-
-        nnodes = codesize = 0
-        _compute_sizes(self.root)
-        print(nnodes)
-        self._values = self._prepare_predictions(nnodes)
-        self._thresholds = np.empty(nnodes)
-        self._code = np.empty(codesize, np.int32)
-
-        code_ptr = node_idx = 0
-        _compile_node(self.root)
-
-    def _compute_descriptions(self):
-        def _compute_subtree(node):
-            for i, child in enumerate(node.children):
-                if child is None:
-                    continue
-                child.parent = node
-                # These classes are friends
-                # pylint: disable=protected-access
-                node._set_child_descriptions(child, i, conditions)
-                old_cond = conditions.get(node.attr)
-                conditions[node.attr] = child.condition
-                _compute_subtree(child)
-                if old_cond is not None:
-                    conditions[node.attr] = old_cond
-                else:
-                    del conditions[node.attr]
-
-        conditions = OrderedDict()
-        self.root.parent = None
-        _compute_subtree(self.root)
-
-    def predict_proba(self, data):
-        return self(data, ret=TreeModelInterface.Probs)
 
     
-class UncertanTree(Learner):
+class UncertainTreeLearner(Learner):
     """
     Tree inducer with proper handling of nominal attributes and binarization.
 
@@ -312,7 +57,7 @@ class UncertanTree(Learner):
     Returns:
         instance of OrangeTreeModel
     """
-    __returns__ = UncertanTreeModel
+    __returns__ = TreeModel
 
     # Binarization is exhaustive, so we set a limit on the number of values
     MAX_BINARIZATION = 16
@@ -412,15 +157,35 @@ class UncertanTree(Learner):
                 len(class_var.values), self.min_samples_leaf)
             if best_score == 0:
                 return REJECT_ATTRIBUTE
+            
+            temp = []
+            sorted_col_x = sorted(col_x)
+            for X, meta in zip(data.X, data.metas):
+                temp.append([X[attr_no], meta[attr_no]])
+            temp = np.array(temp)
+            temp = temp[temp[:, 0].argsort()]
+            print("_______")
+            print(attr_no, "X     meta")
+            print(temp)
+            index_best_cut = sorted_col_x.index(best_cut) #TODO problem if 2 values are equal i think
+            print("orig best cut", best_cut)
+            best_cut = (sorted_col_x[index_best_cut] + sorted_col_x[index_best_cut+1]) * 0.5
+            # print("L best_cut R", temp[index_best_cut], best_cut, temp[index_best_cut+1])
+            offset = (-temp[index_best_cut][1] + temp[index_best_cut+1][1]) * 0.5
+            best_cut = best_cut + offset
+            print("L best_cut R", temp[index_best_cut], best_cut, temp[index_best_cut+1])
+            
             best_score *= non_nans / len(col_x)
             branches = np.full(len(col_x), -1, dtype=int)
             mask = ~np.isnan(col_x)
             branches[mask] = (col_x[mask] > best_cut).astype(int)
+            print(branches)
             node = NumericNode(attr, attr_no, best_cut, None)
             return best_score, node, branches, 2
 
         #######################################
         # The real _select_attr starts here
+        print("----------new branch---------------")
         is_sparse = sp.issparse(data.X)
         domain = data.domain
         class_var = domain.class_var
@@ -439,6 +204,16 @@ class UncertanTree(Learner):
         return best_res
 
     def _build_tree(self, data, active_inst, level=1):
+        # print(type(data),data, "data")
+        # print(type(active_inst),active_inst, "active_inst")
+        # print()
+        # https://stackoverflow.com/a/3844832
+        from itertools import groupby
+        
+        def _all_equal(iterable):
+            g = groupby(iterable)
+            return next(g, True) and not next(g, False)
+
         """Induce a tree from the given data
 
         Returns:
@@ -453,6 +228,8 @@ class UncertanTree(Learner):
             node, branches, n_children = Node(None, None, distr), None, 0
         else:
             node, branches, n_children = self._select_attr(node_insts)
+            if branches is not None and _all_equal(branches) and len(branches) > 1:
+                node, branches, n_children = Node(None, None, distr), None, 0
         node.subset = active_inst
         if branches is not None:
             node.children = [
@@ -471,6 +248,10 @@ class UncertanTree(Learner):
                              format(self.MAX_BINARIZATION))
 
         active_inst = np.nonzero(~np.isnan(data.Y))[0].astype(np.int32)
+        # print(data.domain)
+        # print(data.X)
+        # print(data.Y)
+        print("fit()") 
         root = self._build_tree(data, active_inst)
         if root is None:
             distr = distribution.Discrete(data, data.domain.class_var)
@@ -478,7 +259,7 @@ class UncertanTree(Learner):
                 distr[:] = 1
             root = Node(None, 0, distr)
         root.subset = active_inst
-        model = UncertanTreeModel(data, root)
+        model = TreeModel(data, root)
         return model
 
 class OWUncertaintyTree(OWBaseLearner):
@@ -489,7 +270,7 @@ class OWUncertaintyTree(OWBaseLearner):
     priority = 30
     keywords = "tree, uncertainty tree"
 
-    LEARNER = UncertanTree
+    LEARNER = UncertainTreeLearner
 
     binary_trees = Setting(True)
     limit_min_leaf = Setting(True)
@@ -547,13 +328,11 @@ class OWUncertaintyTree(OWBaseLearner):
                 self.limit_majority])
 
     def create_learner(self):
-        print("create_learner()")
         # pylint: disable=not-callable
         return self.LEARNER(**self.learner_kwargs())
 
     def get_learner_parameters(self):
         # from Orange.widgets.report import plural_w
-        print("get_learner_parameters()")
         items = OrderedDict()
         items["Pruning"] = ", ".join(s for s, c in (
             (f'at least {self.min_leaf} '
